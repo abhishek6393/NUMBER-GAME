af6ad9a793d197ddfcf97765c312659a1b043530
Abhishek kumar verma <145225500+abhishek6393@users.noreply.github.com>
2024-02-05 15:25:19 +0530

    Update number_game.java

--- number_game.java
+++ number_game.java
@@ -1,5 +1,35 @@
-package internship_work;
+import java.util.Scanner;
+import java.util.Random;
 
-public class number_game {
+public class RandomNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
+        int lowerBound = 1;
+        int upperBound = 100;
+        int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+
+        System.out.println("Welcome to the Random Number Game!");
+        System.out.println("Guess the number between " + lowerBound + " and " + upperBound);
+
+        int guess;
+        int attempts = 0;
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < targetNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > targetNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+            }
+        } while (guess != targetNumber);
+
+        scanner.close();
+    }
 }
